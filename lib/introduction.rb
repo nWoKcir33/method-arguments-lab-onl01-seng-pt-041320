bc7e2d75ab287b29a16d4c74c1405dc4f86417c9
Rick Lim <rick.jason.a.lim@gmail.com>
2020-04-22 17:17:10 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,8 +4,8 @@ end
 
 introduction("Josh")
 
-def introduction(name, language)
+def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction("Dan", "Ember.js")
+introduction_with_language("Dan", "Ember.js")
